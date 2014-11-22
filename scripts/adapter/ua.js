32aa1eee22f8cb1a43dcb2990b7b3619964be201
dominik.matt <dominik.matt@massiveart.com>
2014-11-22 15:52:14 +0100

    initialize commit

---
+++ scripts/adapter/ua.js
@@ -0,0 +1,161 @@
+
+var uaTrackingJS = function() {
+
+    /**
+     * initialize
+     */
+    this.init = function (code, url, pageview) {
+        ga('create', code, url);
+        if(pageview === true) {
+            this.pageview();
+        }
+    }.bind(this),
+
+    /**
+     * includes the ua analytics js
+     */
+    this.appendAnalyticsJs = function () {
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+    }
+
+    return {
+        init: this.init,
+        appendAnalyticsJs: this.appendAnalyticsJs,
+        pageview: this.pageview,
+        event: this.event,
+        eCommerce: this.eCommerce
+    }
+};
+
+/**
+ * uaTrackingJS
+ *
+ * @param page
+ * @param title
+ */
+uaTrackingJS.prototype.pageview = function(page, title) {
+    var options = {};
+
+    if(page) {
+        options.page = page;
+    }
+
+    if(title) {
+        options.title = title;
+    }
+
+
+    ga('send', 'pageview', options);
+};
+
+/**
+ * uaTrackingJS
+ *
+ * @param category
+ * @param action
+ * @param label
+ * @param value
+ */
+uaTrackingJS.prototype.event = function(category, action, label, value) {
+    var options = {
+        'hitType': 'event',
+        eventCategory: category,
+        eventAction: action
+    };
+
+    if(label && label != '') {
+        options.label = label;
+    }
+
+    if(value && value != '') {
+        options.eventValue = value;
+    }
+
+    ga('send', options);
+};
+
+/**
+ * eCommerce plugin
+ *
+ * @type {{generate: Function, addTransaction: Function, addItems: Function}}
+ */
+uaTrackingJS.prototype.eCommerce = {
+
+    trackingJs: null,
+    ec: null,
+
+    generate: function(trackingJS, ec) {
+        this.trackingJS = trackingJS;
+        this.ec = ec;
+        if(ec.transaction.id && ec.transaction.id != '') {
+            //enable ecommerce
+            ga('require', 'ecommerce');
+            this.addTransaction();
+            this.addItems();
+            this.send();
+        } else {
+            this.trackingJS.helper.error('Transaction ID is required');
+        }
+    },
+
+    addTransaction: function() {
+
+        var options = {
+            id: this.ec.transaction.id,
+            revenue: this.ec.getRevenue()
+        };
+
+        if(this.ec.transaction.affiliation) {
+            options.affiliation = this.ec.transaction.affiliation;
+        }
+
+        if(this.ec.transaction.shipping) {
+            options.shipping = this.ec.transaction.shipping;
+        }
+
+        if(this.ec.transaction.tax) {
+            options.tax = this.ec.transaction.tax;
+        }
+
+
+        ga('ecommerce:addTransaction', options);
+    },
+
+    addItems: function() {
+        $.each(this.ec.getItems(), function(key, item) {
+            if (item.id && item.name && item.id != '' && item.name != '') {
+                var options = {
+                    'id': item.id,
+                    'name': item.name
+                };
+
+                if(item.sku && item.sku != '') {
+                    options.sku = item.sku
+                }
+
+                if(item.category && item.category != '') {
+                    options.category = item.category;
+                }
+
+                if(item.price && !isNaN(item.price)) {
+                    options.price = item.price;
+                }
+
+                if(item.quantity && !isNaN(item.quantity)) {
+                    options.quantity = item.quantity;
+                }
+
+                ga('ecommerce:addItem', options);
+            } else {
+                this.helper.error('ID and NAME of a item are required');
+            }
+        });
+    },
+
+    send: function() {
+        ga('ecommerce:send');
+    }
+};
---
+++ scripts/trackingjs.js
@@ -0,0 +1,179 @@
+var trackingJS = function(options) {
+
+    this.tracking = null;
+
+    /**
+     * default settings
+     *
+     * @author Dominik Matt <dma@massiveart.com>
+     */
+    settings = $.extend({
+        type: 'ua',
+        analyticsCode: '',
+        url: 'auto',
+        pageview: true,
+        debug: true
+    }, options);
+
+    /**
+     * init
+     *
+     * @author Dominik Matt <dma@massiveart.com>
+     */
+    this.init = function() {
+        this.loadAdapter();
+        if(this.tracking && typeof this.tracking == 'object') {
+            this.tracking.appendAnalyticsJs();
+            this.tracking.init(settings.analyticsCode, settings.url, settings.pageview);
+        } else {
+            throw 'Tracking type not loaded';
+        }
+
+    }.bind(this);
+
+    /**
+     * loadAdapter
+     *
+     * @author Dominik Matt <dma@massiveart.com>
+     */
+    this.loadAdapter = function() {
+        if(settings.type != '') {
+            var className = settings.type + 'TrackingJS'
+            if(typeof window[className] == 'function') {
+                this.tracking = new window[className]();
+                return true;
+            }
+        }
+
+        return false;
+    }.bind(this);
+
+    /**
+     * pageview
+     *
+     * @author Dominik Matt <dma@massiveart.com>
+     */
+    this.pageview = function(page, title) {
+        this.tracking.pageview(page, title);
+    }.bind(this);
+
+    /**
+     * event
+     *
+     * @author Dominik Matt <dma@massiveart.com>
+     */
+    this.event = function(category, action, label, value) {
+        this.helper.info('Send event:', category, action, label, value);
+        this.tracking.event(category, action, label, value);
+    }.bind(this);
+
+    this.helper = {
+        error: function(msg){
+            console.error('trackingJS: ' + msg);
+        },
+
+        info: function(msg){
+            if(settings.debug) {
+                console.info('trackingJS: ' + msg);
+            }
+        }
+    };
+
+    /**
+     * register eCommerce Plugin
+     *
+     * @type {function(this:trackingJS)}
+     */
+    this.registerEcommerce = function() {
+        if(typeof this.eCommerce == 'function') {
+            this.helper.info('Register eCommerce tracking');
+            return new this.eCommerce(this);
+        } else {
+            this.helper.error('eCommerce plugin not found');
+            return false;
+        }
+    }.bind(this);
+
+    this.init();
+
+    return {
+        pageview: this.pageview,
+        event: this.event,
+        registerEcommerce: this.registerEcommerce
+    };
+
+};
+
+/**
+ * eCommerce plugin
+ *
+ * @param trackingJS
+ * @author Dominik Matt <dma@massiveart.com>
+ */
+trackingJS.prototype.eCommerce = function(trackingJS) {
+
+    this.transaction = {
+        'id': null,             // Transaction ID. Required.
+        'affiliation': '',      // Affiliation or store name.
+        'revenue': null,        // Grand Total.
+        'shipping': null,       // Shipping.
+        'tax': null             // Tax.
+    };
+
+    this.items = [];
+
+    this.setId = function(id) {
+        this.transaction.id = id;
+    };
+
+    this.setAffiliation = function(affiliation) {
+        this.transaction.affiliation = affiliation;
+    };
+
+    this.setShipping = function(shipping) {
+        this.transaction.shipping = shipping;
+    };
+
+    this.setTax = function(tax) {
+        this.transaction.tax = tax;
+    };
+
+    this.getRevenue = function() {
+        var total = 0;
+        $.each(this.getItems(), function(key, item) {
+            if(item.price) {
+                total += item.price;
+            }
+        });
+
+        return total;
+    };
+
+    this.addItem = function(item) {
+        trackingJS.helper.info('Add item to ecommerce:', item);
+        if(typeof item == 'object') {
+            this.items.push(item);
+        } else {
+            trackingJS.helper.error('Item must be an object');
+        }
+    };
+
+    this.send = function() {
+        trackingJS.tracking.eCommerce.generate(trackingJS, this);
+    }.bind(this);
+
+};
+
+/**
+ * getItems
+ * returns a object with all items
+ *
+ * @returns {Array}
+ */
+trackingJS.prototype.eCommerce.prototype.getItems = function() {
+    return this.items;
+};
+
+
+
+
