8f356a4df031bfec491e4f00c92c19056b78f2c8
Daniel Metzler <daniel.metzler@massiveart.com>
2016-05-02 15:12:19 +0200

    added check to run google analytics script only once

--- scripts/adapter/ua.js
+++ scripts/adapter/ua.js
@@ -44,12 +44,14 @@ var uaTrackingJS = function(trackingJSOptions, trackingJSHelper) {
      * includes the ua analytics js
      */
     this.appendAnalyticsJs = function () {
-        /* jshint ignore:start */
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-            (i[r].q=i[r].q||[]).push(arguments);},i[r].l=1*new Date();a=s.createElement(o),
-            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m);
-        })(window,document,'script','//www.google-analytics.com/analytics.js','ga'); 
-        /* jshint ignore:end */
+        if (typeof(ga) === 'undefined') {
+            /* jshint ignore:start */
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments);},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m);
+            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+            /* jshint ignore:end */
+        }
     };
 };
 
